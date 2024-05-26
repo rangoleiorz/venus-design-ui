1979c0bb3407758cc80156cda244efa400d6fef7
rangoleiorz <834912674@qq.com>
2024-05-26 12:08:16 +0800

    perf(types): 调整类型文件路径指向

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,7 @@
 // Definitions by: akki-jat <https://github.com/akki-jat>
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
-export * from './ant-design-vue';
+export * from './venus-design-ui';
 
-import * as Antd from './ant-design-vue';
+import * as Antd from './venus-design-ui';
 export default Antd;
